decdf5ad12f4e8440c6e4e434a256591844c611d
Ilya <ilyadvyu@gmail.com>
2026-01-07 17:52:57 +0300

    1.1 edges added

--- main.py
+++ main.py
@@ -2,29 +2,38 @@ import arcade
 from SeedNoiseGenerator import SeedNoiseGenerator
 from random import randint
 
-SCREEN_WIDTH = 1000
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "mindustry0.5"
 
 TILE_SIZE = 10
-COLS = SCREEN_WIDTH // TILE_SIZE + 1
-ROWS = SCREEN_HEIGHT // TILE_SIZE + 1
+COLS = SCREEN_WIDTH // TILE_SIZE
+ROWS = SCREEN_HEIGHT // TILE_SIZE
 
 
 class MyGame(arcade.Window):
     def __init__(self, WIDTH, HEIGHT, SCREEN_TITLE):
         super().__init__(WIDTH, HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK)
-        seed = 58 # randint(1, 1000) #1001
+        seed = randint(1, 1000) ##100158
         self.generator = SeedNoiseGenerator(seed)
         self.world = {}
         print(self.generator.get_seed())
 
+    def edges(self):
+        for r in range(1, ROWS + 1):
+            for c in range(1, COLS + 1):
+                x = TILE_SIZE * (c - 1)
+                y = TILE_SIZE * (r - 1)
+                if r == 1 or r == ROWS or c == 1 or c == COLS:
+                    self.world[(x, y)] = (40, 20, 30)
+
     def create_snow_and_water(self):
-        for r in range(1, ROWS):
-            for c in range(1, COLS):
-                x = c * TILE_SIZE - TILE_SIZE // 2
-                y = r * TILE_SIZE - TILE_SIZE // 2
+
+        for r in range(2, ROWS):
+            for c in range(2, COLS):
+                x = TILE_SIZE * (c - 1)
+                y = TILE_SIZE * (r - 1)
                 snow = self.generator.noise(x, y, octaves=4, persistence=0.5, lacunarity=2.0)
                 water = self.generator.noise(x, y, octaves=2, seed_offset=1000)
                 if snow > 0.565:
@@ -34,12 +43,13 @@ class MyGame(arcade.Window):
                 if snow < 0.56 and water < 0.66:
                     color = (120, 120, 120)  # Камень
                 self.world[(x, y)] = color
+        self.edges()
 
     def create_copper(self):
-        for r in range(1, ROWS):
-            for c in range(1, COLS):
-                x = c * TILE_SIZE - TILE_SIZE // 2
-                y = r * TILE_SIZE - TILE_SIZE // 2
+        for r in range(2, ROWS):
+            for c in range(2, COLS):
+                x = TILE_SIZE * (c - 1)
+                y = TILE_SIZE * (r - 1)
                 copper = self.generator.noise(x, y, octaves=1, seed_offset=255)
                 if self.world[(x, y)] != (48, 15, 240) and copper > 0.7:
                     self.world[(x, y)] = (244, 132, 5)
@@ -48,10 +58,10 @@ class MyGame(arcade.Window):
         self.clear()
         self.create_snow_and_water()
         self.create_copper()
-        for r in range(1, ROWS):
-            for c in range(1, COLS):
-                x = c * TILE_SIZE - TILE_SIZE // 2
-                y = r * TILE_SIZE - TILE_SIZE // 2
+        for r in range(1, ROWS + 1):
+            for c in range(1, COLS + 1):
+                x = TILE_SIZE * (c - 1)
+                y = TILE_SIZE * (r - 1)
                 arcade.draw_lbwh_rectangle_filled(x, y, TILE_SIZE, TILE_SIZE, self.world[(x, y)])
 
 
