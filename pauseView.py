edfaf871692cdb5bbd1b080b747cab704b4da630
Fenix46r <renatkozhoev2009@gmail.com>
2026-02-02 16:34:00 +0300

    Update pauseView.py

--- pauseView.py
+++ pauseView.py
@@ -3,6 +3,7 @@ from arcade.gui import UITextureButton, UIAnchorLayout, UIBoxLayout, UISpace, UI
 from baseView import BaseView
 import json
 import pickle
+import time
 from random import randint
 from datetime import date
 from CustomButton import CustomButton
@@ -16,19 +17,23 @@ class PauseView(BaseView):
 
     def create_widget(self):
         c_space = UISpace(width=800, height=100, color=(0, 0, 0, 0))
-        self.button_continue = CustomButton(768 * 0.4, 248 * 0.4, "CONTINUE", 0.362, 0.362,
-                                            self.window.sprites['button_n'],
-                                            self.window.sprites['button_a'],
-                                            self.window.sprites['button_t'])
+        self.button_continue = CustomButton(self.width * 0.2, self.height * 0.1, "CONTINUE", self.width * 0.00023,
+                                            self.width * 0.00023,
+                                            self.window.textures['button_n'],
+                                            self.window.textures['button_a'],
+                                            self.window.textures['button_t'])
 
-        self.button_settings = CustomButton(768 * 0.4, 248 * 0.4, "SETTINGS", 0.362, 0.362,
-                                            self.window.sprites['button_n'],
-                                            self.window.sprites['button_a'],
-                                            self.window.sprites['button_t'])
+        self.button_settings = CustomButton(self.width * 0.2, self.height * 0.1, "SETTINGS", self.width * 0.00023,
+                                            self.width * 0.00023,
+                                            self.window.textures['button_n'],
+                                            self.window.textures['button_a'],
+                                            self.window.textures['button_t'])
 
-        self.button_exit = CustomButton(768 * 0.4, 248 * 0.4, "EXIT", 0.4, 0.4, self.window.sprites['button_n'],
-                                        self.window.sprites['button_a'],
-                                        self.window.sprites['button_t'])
+        self.button_exit = CustomButton(self.width * 0.2, self.height * 0.1, "EXIT", self.width * 0.00023,
+                                        self.width * 0.00023,
+                                        self.window.textures['button_n'],
+                                        self.window.textures['button_a'],
+                                        self.window.textures['button_t'])
         self.c_anchor = UIAnchorLayout()
         self.cv_layout = UIBoxLayout(vertical=True, space_between=10)
 
@@ -45,6 +50,10 @@ class PauseView(BaseView):
         self.button_exit.on_click = lambda event: self.exit_triggered(event)
         self.button_settings.on_click = lambda event: self.settings_triggered(event)
 
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.ESCAPE:
+            self.continue_triggered(True)
+
     def continue_triggered(self, event):
         self.window.game_view.pause = False
         self.window.show_view(self.window.game_view)
@@ -52,9 +61,9 @@ class PauseView(BaseView):
     def exit_triggered(self, event):
         self.data = self.window.game_view.data
         self.data['date'] = str(date.today())
-        with open(f"saves/{self.data.get("name")}.json", "w", encoding="utf-8") as file:
+        with open(f"saves/{self.data.get('name')}.json", "w", encoding="utf-8") as file:
             json.dump(self.data, file, indent=4, ensure_ascii=False)
-        with open(f"saves/{self.data.get("name")}.sv", "wb") as file:
+        with open(f"saves/{self.data.get('name')}.sv", "wb") as file:
             pickle.dump(self.data, file)
         self.window.is_game = False
         self.window.show_view(self.window.menu_view)
